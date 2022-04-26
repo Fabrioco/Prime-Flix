034f2e115ba306016a6f9c585f444d8f90195d66
Fabrioco <Fabriciooliveiralopes50@gmail.com>
2022-04-26 13:49:56 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,46 +1,30 @@
 import './App.css';
+
 import { Component } from 'react';
+
+
+import {loadPosts} from './utils/load-posts'
 
 class App extends Component {
   state = {
-    posts: [ ]
+    posts: []
     };
 
-    componentDidMount() {
+    async componentDidMount() {
       this.loadPosts()
     }
 
     loadPosts = async () => {
-      const postsReponse = fetch('https://jsonplaceholder.typicode.com/posts')
-      const photosReponse = fetch('https://jsonplaceholder.typicode.com/photos')
-
-      const [posts, photos] = await Promise.all([postsReponse, photosReponse])
-
-      const postsJson = await posts.json()
-      const photosJson = await photos.json()
-
-      const postsAndPhotos = postsJson.map((post, index) => {
-        return{ ...post, cover: photosJson[index].url }
-      })
-
-      this.setState({ posts: postsAndPhotos })
+      const postsAndPhotos = await loadPosts ();
+      this.setState({ posts: postsAndPhotos });
     }
 
   render() {
     const { posts } = this.state;
+
     return(
       <section className='container'>
-        <div className="posts">
-        {posts.map(post => (
-          <div className='post'>
-            <img src={post.cover} alt= {post.title} />
-           <div key={post.id} className="post-content">
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-          </div>
-      ))}
-        </div>
+        <Posts posts={posts} />
       </section>
     );
   }
