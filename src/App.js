aaec55e0c605d3a9dd2c1e151849ed35d4fdf619
Fabricio Lopes <Fabriciooliveiralopes50@gmail.com>
2022-06-01 21:45:53 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-
-function App() {  
+function App() { 
   return (
     <div className="App">
       <header className='App-header'>
-        
       </header>
     </div>
     )
