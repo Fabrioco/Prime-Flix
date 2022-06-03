30c338d7a5d679c400d2280164f5accb43bfcf55
Fabricio Lopes <Fabriciooliveiralopes50@gmail.com>
2022-06-03 15:29:16 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import P from 'prop-types';
+import { useEffect, useState } from 'react';
 import './App.css';
-function App() { 
+
+
+const Button = ({ incrementButton }) => {
+  return <button onClick={() => incrementButton(10)}>+</button>
+}
+
+Button.propTypes = {
+  incrementButton:P.func,
+};
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1)
+  }
+
   return (
     <div className="App">
-      <header className='App-header'>
-      </header>
+      <p>Teste 3</p>
+      <h1>C1: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
     </div>
     )
 }
