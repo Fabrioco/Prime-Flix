8736db83808e82ad22e8be1bf1b93edd988a707e
Fabricio Lopes <Fabriciooliveiralopes50@gmail.com>
2022-07-01 22:36:14 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import './App.css';
+
+const Bemvindo = (props) => {
+  return(
+    <div>
+      <h2>Bem-vindo {props.nome} </h2>
+      <h3>Tenho {props.idade} anos</h3>
+    </div>
+  );
+}
 
 function App() {
 
   return (
-    <div className="App">
-      
-      </div>
-);
+    <div>
+      Olá mundo!
+      <Bemvindo nome="Lucas" idade="19"/>
+      <Bemvindo nome="paulo" idade="25"/>
+    </div>
+  );
 }
 
 export default App;
