e91a311d23e6f28381fd9082796b318b405af831
Fabricio Lopes <Fabriciooliveiralopes50@gmail.com>
2022-07-26 17:24:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React, {useState} from "react";
 
 function App(){
-    const [nome, setNome] = useState('Fabricio')
-
-    function Mudar(){
-        setNome("Pedro");
-    }
-
-    function Voltar(){
-        setNome('Fabricio');
-    }
-
+    
     return(
         <div>
-            <h1>{nome}</h1>
-            <button type="button" onClick={Mudar} >Mudar</button>
-            <button type="button" onClick={Voltar} >Voltar</button>
+           
         </div>
     )
 }
