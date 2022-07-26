5ab42932395e2d74e875e8379c8cb8dde369940f
Fabricio Lopes <Fabriciooliveiralopes50@gmail.com>
2022-07-26 20:07:41 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, {} from "react";
+import React, { useState } from "react";
 
 function App(){
-    
+    const [contador, setContador] = useState(0)
+    function Aumentar(){
+       setContador(contador + 1)
+    }
+    function Diminuir(){
+        setContador(contador - 1)
+        if(contador < 0){
+            alert('o contador n passa de 0')
+            return;
+        } 
+    }
+
+
     return(
         <div>
-           
+        <h2>{contador}</h2>
+        <button type="button" onClick={Diminuir}>-</button>
+        <button type="button" onClick={Aumentar} >+</button>
         </div>
     )
 }
