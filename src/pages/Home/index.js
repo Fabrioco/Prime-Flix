dff8272e7c1d6bfa1ad144ba282a69c47d2f8a11
Fabricio Lopes <Fabriciooliveiralopes50@gmail.com>
2022-08-01 05:33:18 -0300

    a

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,12 @@
 import { useEffect,useState } from "react";
-import api from '../../services/api'
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css'
 //URL da Api: https://api.themoviedb.org/3/movie/now_playing?api_key=d990f125e042de6239b35dc9f8a09a89&language=pt-BR
 
 function Home(){
     const [filmes, setFilmes] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(()=>{
         async function loadFilmes(){
@@ -15,17 +18,36 @@ function Home(){
                 }
             })
 
-            console.log(response.data.results
-                )
+            //console.log(response.data.results.slice(0, 10))
+            setFilmes(response.data.results.slice(0, 10))
+            setLoading(false)
         }
 
         loadFilmes();
 
     },[])
 
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>carregando filmes...</h2>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>Bem vindo ao Home</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme)=> {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Filme from './pages/Filme';
+import Erro from './pages/Erro';
+import Favoritos from './pages/Favoritos';
 
 import Header from './components/Header'
 
@@ -12,6 +14,8 @@ function RoutesApp(){
             <Routes>
                 <Route path='/' element={ <Home /> } />
                 <Route path='/Filme/:id' element={ <Filme /> } />
+                <Route path='/Favoritos' element= { <Favoritos /> } />
+                <Route path='*' element={ <Erro/> } />
             </Routes>
         </BrowserRouter>
     )
