a16743d586d503a9641ab81c0842b2fe74ebe10a
Fabricio Lopes <Fabriciooliveiralopes50@gmail.com>
2022-08-05 19:10:18 -0300

    a

---
+++ Curso/Projeto PrimeFlix/src/pages/Filme/index.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import './filme.css';
+
+import { toast } from 'react-toastify'
+
+function Filme(){
+    const { id } = useParams();
+    const navigation = useNavigate();
+
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: 'd990f125e042de6239b35dc9f8a09a89',
+                    language: 'pt-BR'
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false)
+            })
+            .catch(()=>{
+                //console.log("filme n encontrado")
+                navigation("/", { replace: true });
+                return;
+            })
+        }
+        loadFilme();
+
+        return () => {
+            console.log('Component foi desmontado')
+        }
+    }, [navigation, id])
+
+    function salvaFilme(){
+        const minhaLista = localStorage.getItem("@primeflix");
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some( (filmesSalvo) => filmesSalvo.id === filme.id );
+        
+        if(hasFilme){
+            toast.warn("Esse filme já está na sua lista")
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        toast.success("Filme salvo com sucesso!")
+
+    }
+
+    if(loading){
+        return(
+            <div className='filme-info'>
+                <h1>CARREGANDO DETALHES...</h1>
+            </div>
+        )
+    }
+
+    return(
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} /10</strong>
+                <div className='area-buttons'>
+                    <button onClick={salvaFilme}>Salvar</button>
+                    <button>
+                        <a target='blank' rel='external' href={`https://youtube.com/results?search_query=${filme.title} trailer`}>
+                            Trailer
+                        </a>
+                    </button>
+                </div>
+        </div>
+    )
+}
+
+export default Filme;
